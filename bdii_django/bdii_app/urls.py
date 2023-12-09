96e68eef9b958b36e8da9916187e61ccc27fc18e
Rui Rosa <96315477+RuiRosaa@users.noreply.github.com>
2023-12-09 21:37:29 +0000

    Dashboard, registo encomenda inicio

--- bdii_django/bdii_app/urls.py
+++ bdii_django/bdii_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('registo_encomenda/', views.registo_encomenda, name='registo_encomenda'),
 ]
--- bdii_django/bdii_app/views.py
+++ bdii_django/bdii_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def registo_encomenda(request):
+    return render(request, 'registo_encomenda.html')
