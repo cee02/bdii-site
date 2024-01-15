c153dadaefee750035d4199c46a46b4acd1cd1b3
AndrePinto27 <111457330+andre79d@users.noreply.github.com>
2024-01-15 13:01:40 +0000

    Urls fix

--- bdii_django/bdii_app/urls.py
+++ bdii_django/bdii_app/urls.py
@@ -4,14 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='c_logout'),
+    path('', views.logout, name='c_logout'),
     path('error_page/<str:error_message>/', views.error_page, name='error_page'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('registo_encomenda/', views.registo_encomenda, name='registo_encomenda'),
-    path('producao_equipamentos/', views.producao_equipamentos, name='producao_equipamentos'),
-    path('registar_equipamento/', views.registar_equipamento, name='registar_equipamento'),
-    path('vendas_equipamentos/', views.vendas_equipamentos, name='vendas_equipamentos'),
-    path('gestao_clientes/', views.gestao_clientes, name='gestao_clientes'),
+    path('dashboard/', views.dashboard, name='bdii_dashboard'),
+    path('registo_encomenda/', views.registo_encomenda, name='bdii_registo_encomenda'),
+    path('producao_equipamentos/', views.producao_equipamentos, name='bdii_producao_equipamentos'),
+    path('registar_equipamento/', views.registar_equipamento, name='bdii_registar_equipamento'),
+    path('vendas_equipamentos/', views.vendas_equipamentos, name='bdii_vendas_equipamentos'),
+    path('gestao_clientes/', views.gestao_clientes, name='bdii_gestao_clientes'),
     path('delete-cliente/<int:cliente_id>/', views.delete_cliente, name='delete_cliente'),
 
 ]
